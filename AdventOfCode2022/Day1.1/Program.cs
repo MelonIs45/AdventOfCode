30d35594dba66fc4fa65990934a152889771d4ac
MelonIs45 <ryanpietras45@gmail.com>
2022-12-01 17:28:27 +0000

    2022 day 1 done

---
+++ AdventOfCode2022/Day1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            string[] split = string.Join(",", input).Split(",,");
+            int highest = 0;
+
+            foreach (string s in split)
+            {
+                int total = 0;
+
+                foreach (string num in s.Split(","))
+                    total += int.Parse(num);
+
+                if (total > highest) 
+                    highest = total;
+            }
+
+            Console.WriteLine(highest);
+        }
+    }
+}
---
+++ AdventOfCode2022/Day1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Day1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            string[] split = string.Join(",", input).Split(",,");
+            List<int> nums = new List<int>();
+
+            foreach (string s in split)
+            {
+                int total = 0;
+
+                foreach (string num in s.Split(","))
+                    total += int.Parse(num);
+
+                nums.Add(total);
+            }
+
+            nums.Sort();
+            int count = nums.Count;
+            Console.WriteLine(nums[count - 1] + nums[count - 2] + nums[count - 3]);
+        }
+    }
+}
