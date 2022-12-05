1a2146bd4d74fdd25eedf120b599358d95d0a6a2
MelonIs45 <ryanpietras45@gmail.com>
2022-12-05 14:45:04 +0000

    cleanup

--- AdventOfCode2022/Day5/Program.cs
+++ AdventOfCode2022/Day5/Program.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.Metrics;
-using System.Diagnostics;
-using System.Numerics;
-using System.Threading;
-
-namespace Day5
+﻿namespace Day5
 {
     internal class Program
     {
-        const int TALLEST_HEIGHT = 8;
+        private const int TallestHeight = 8;
+        private const int AmountOfRows = 9;
 
         static void Main(string[] args)
         {
@@ -19,19 +13,19 @@ namespace Day5
 
         static (string, string) Solve()
         {
-            string res = "";
-            string res2 = "";
+            string resPartOne = "";
+            string resPartTwo = "";
             string[] input = File.ReadAllLines("input.txt");
-            string[][] stacks = new string[TALLEST_HEIGHT][];
+            string[][] stacks = new string[TallestHeight][];
 
-            Stack<string>[] stackOfStacks = new Stack<string>[9];
-            Stack<string>[] stackOfStacksPartTwo = new Stack<string>[9];
+            Stack<string>[] stackOfStacks = new Stack<string>[AmountOfRows];
+            Stack<string>[] stackOfStacksPartTwo = new Stack<string>[AmountOfRows];
 
-            for (int i = 0; i < TALLEST_HEIGHT; i++)
+            for (int i = 0; i < TallestHeight; i++)
             {
                 string line = input[i];
                 int length = line.Length;
-                string[] stack = new string[9];
+                string[] stack = new string[AmountOfRows];
 
                 for (int j = 0; j < length; j += 4)
                     if (line[j + 1] != ' ')
@@ -40,11 +34,11 @@ namespace Day5
                 stacks[i] = stack;
             }
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < AmountOfRows; i++)
             {
                 Stack<string> stack = new Stack<string>();
                 for (int j = 7; j >= 0; j--)
-                    if (stacks[j][i] != null)
+                    if (!string.IsNullOrEmpty(stacks[j][i]))
                         stack.Push(stacks[j][i]);
                 
                 stackOfStacks[i] = stack;
@@ -54,11 +48,8 @@ namespace Day5
             }
 
             (int, int, int)[] instructions = new (int, int, int)[input.Length - 10];
-
-            for (int i = 0; i < input.Length-10; i++)
-            {
-                instructions[i] = (int.Parse(input[i + 10].Split(" ")[1]), int.Parse(input[i + 10].Split(" ")[3]), int.Parse(input[i + 10].Split(" ")[5]));
-            }
+            for (int i = 0; i < input.Length- AmountOfRows - 1; i++)
+                instructions[i] = (int.Parse(input[i + AmountOfRows + 1].Split(" ")[1]), int.Parse(input[i + AmountOfRows + 1].Split(" ")[3]), int.Parse(input[i + AmountOfRows + 1].Split(" ")[5]));
 
             foreach ((int, int, int) instruction in instructions)
             {
@@ -70,19 +61,16 @@ namespace Day5
                 }
 
                 for (int i = 0; i < instruction.Item1; i++)
-                {
                     stackOfStacksPartTwo[instruction.Item3-1].Push(tempStack.Pop());
-                }
-                    
             }
 
             for (int i = 0; i < stackOfStacks.Length; i++)
             {
-                res += stackOfStacks[i].Peek();
-                res2 += stackOfStacksPartTwo[i].Peek();
+                resPartOne += stackOfStacks[i].Peek();
+                resPartTwo += stackOfStacksPartTwo[i].Peek();
             }
 
-            return (res, res2);
+            return (resPartOne, resPartTwo);
         }
     }
 }
