834bcba7c6d64e984067a2319433d162b85ed359
MelonIs45 <ryanpietras45@gmail.com>
2022-12-05 14:53:58 +0000

    more ocd

--- AdventOfCode2022/Day5/Program.cs
+++ AdventOfCode2022/Day5/Program.cs
@@ -35,7 +35,7 @@
             for (int i = 0; i < AmountOfRows; i++)
             {
                 Stack<string> stack = new Stack<string>();
-                for (int j = 7; j >= 0; j--)
+                for (int j = AmountOfRows - 1; j >= 0; j--)
                     if (!string.IsNullOrEmpty(stacks[j][i]))
                         stack.Push(stacks[j][i]);
                 
