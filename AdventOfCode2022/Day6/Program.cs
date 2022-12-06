1a11dc1ec7b2a379cc80b82143414ca733314512
MelonIs45 <ryanpietras45@gmail.com>
2022-12-06 12:19:42 +0000

    day 6 time taken: 1h7m p2 (18s)

---
+++ AdventOfCode2022/Day6/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(4));
+            Console.WriteLine(Solve(14));
+        }
+
+        static int Solve(int markerLength)
+        {
+            string input = File.ReadAllText("input.txt");
+            int counter = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char[] packet = input.Substring(i, markerLength).ToCharArray();
+
+                foreach (char c in packet)
+                {
+                    if (packet.Count(x => x == c) == 1)
+                        counter++;
+                    else
+                        counter = 0;
+                }
+
+                if (counter == markerLength)
+                    return i + markerLength;
+
+                counter = 0;
+            }
+
+            return 0;
+
+
+        }
+    }
+}
